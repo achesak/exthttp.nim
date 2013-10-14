c79bff734834c48a5846af0a55e0101095399799
achesak <achesak@yahoo.com>
2013-10-14 17:25:06 -0500

    First commit.

---
+++ exthttp.nim
@@ -0,0 +1,10 @@
+# Nimrod module with more HTTP functions.
+
+# Written by Adam Chesak.
+# Code released under the MIT open source license.
+
+# Import modules.
+import httpclient
+import strtabs
+
+
